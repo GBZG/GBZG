ff4f0cfe5688aac972ead2923e7ce89cd833be19
otoolz <otool.hf@gmail.com>
2022-11-15 01:01:09 +0900

    [feat] TabView 추가
    TabView UI를 추가합니다.

---
+++ GBZG/View/GBZGTabView.swift
@@ -0,0 +1,30 @@
+//
+//  GBZGTabView.swift
+//  GBZG
+//
+//  Created by KiWoong Hong on 2022/11/15.
+//
+
+import SwiftUI
+
+struct GBZGTabView: View {
+    var body: some View {
+        TabView {
+            ForEach(Tab.allCases, id: \.self) { tab in
+                tab.view
+                    .tabItem {
+                        
+                        // TODO: 추후에 이미지나 시스템 이미지 맞는 걸로 변경
+                        
+                        Label(tab.tabName, systemImage: tab.systemImage)
+                    }
+            }
+        }
+    }
+}
+
+struct GBZGTabView_Previews: PreviewProvider {
+    static var previews: some View {
+        GBZGTabView()
+    }
+}
---
+++ GBZG/View/TabRouter.swift
@@ -0,0 +1,41 @@
+//
+//  TabRouter.swift
+//  GBZG
+//
+//  Created by KiWoong Hong on 2022/11/15.
+//
+
+import SwiftUI
+
+enum Tab: CaseIterable {
+    case home
+    case write
+    case templete
+    
+    var tabName: String {
+        switch self {
+        case .home : return "Home"
+        case .write: return "Write"
+        case .templete: return "Templete"
+        }
+    }
+    
+    // TODO: 추후에 선정 아이콘에 맞는 이미지나 시스템이미지로 변경
+    
+    var systemImage: String {
+        switch self {
+        case .home : return "key.fill"
+        case .write: return "folder.fill"
+        case .templete: return "person.crop.circle.fill"
+        }
+    }
+ 
+    @ViewBuilder
+    var view: some View {
+        switch self {
+        case .home : HomeView()
+        case .write: WriteView()
+        case .templete: TempleteView()
+        }
+    }
+}
