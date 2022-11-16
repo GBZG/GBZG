f52879efe184f3f65078e226bdb0dacb8311347f
otoolz <otool.hf@gmail.com>
2022-11-16 09:49:06 +0900

    [style] GBZGTabRouter 콜론 스페이싱 통일
    GBZGTabRouter 콜론 스페이싱 통일

--- GBZG/Model/GBZGTabRouter.swift
+++ GBZG/Model/GBZGTabRouter.swift
@@ -14,9 +14,9 @@ enum GBZGTab: CaseIterable {
     
     var tabName: String {
         switch self {
-        case .home : return "Home"
-        case .write: return "Write"
-        case .template: return "Template"
+        case .home      : return "Home"
+        case .write     : return "Write"
+        case .template  : return "Template"
         }
     }
     
@@ -24,34 +24,34 @@ enum GBZGTab: CaseIterable {
     
     var enableImage: String {
         switch self {
-        case .home : return "EllipsisVertical"
-        case .write: return "EllipsisVertical"
-        case .template: return "EllipsisVertical"
+        case .home      : return "EllipsisVertical"
+        case .write     : return "EllipsisVertical"
+        case .template  : return "EllipsisVertical"
         }
     }
     
     var disableImage: String {
         switch self {
-        case .home : return "HotelKeys"
-        case .write: return "Pencil"
-        case .template: return "TextBlock"
+        case .home      : return "HotelKeys"
+        case .write     : return "Pencil"
+        case .template  : return "TextBlock"
         }
     }
     
     var tag: Int {
         switch self {
-        case .home : return 0
-        case .write: return 1
-        case .template: return 2
+        case .home      : return 0
+        case .write     : return 1
+        case .template  : return 2
         }
     }
  
     @ViewBuilder
     var view: some View {
         switch self {
-        case .home : HomeView()
-        case .write: WriteView()
-        case .template: TemplateView()
+        case .home      : HomeView()
+        case .write     : WriteView()
+        case .template  : TemplateView()
         }
     }
 }
