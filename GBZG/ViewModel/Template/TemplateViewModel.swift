904b0ce3d9b1650ac9a1f417ad65a485cf36592c
Hyeon-sang "Theo" Lee <88080251+phainestha1@users.noreply.github.com>
2022-12-06 02:44:19 +0900

    [Feat] 폴더 구조를 재구성합니다.

---
+++ GBZG/ViewModel/Template/TemplateViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  TemplateViewModel.swift
+//  GBZG
+//
+//  Created by Noah's Ark on 2022/12/06.
+//
+
+import Foundation
---
+++ GBZG/ViewModel/Write/WriteViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  WriteViewModel.swift
+//  GBZG
+//
+//  Created by Noah's Ark on 2022/12/06.
+//
+
+import Foundation
