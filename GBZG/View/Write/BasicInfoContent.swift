49acc858a809903cee58303837b76bdb13143e09
Hyeon-sang "Theo" Lee <88080251+phainestha1@users.noreply.github.com>
2023-01-11 01:39:59 +0900

    [Feat] WriteView 기본 구조를 구성합니다.

---
+++ GBZG/View/Write/BasicInfoContent.swift
@@ -0,0 +1,68 @@
+//
+//  BasicInfoContent.swift
+//  GBZG
+//
+//  Created by Noah's Ark on 2023/01/11.
+//
+
+import SwiftUI
+
+struct BasicInfoContent<Content: View>: View {
+    let title: String
+    var isCheckboxActive: Bool
+    let content: Content
+
+    init(
+        title: String,
+        isCheckboxActive: Bool = true,
+        @ViewBuilder content: () -> Content
+    ) {
+        self.title = title
+        self.isCheckboxActive = isCheckboxActive
+        self.content = content()
+    }
+    
+    var body: some View {
+        VStack {
+            HStack {
+                titleView
+                content
+            }
+            checkBox
+        }
+    }
+}
+
+private extension BasicInfoContent {
+    var titleView: some View {
+        Text(title)
+            .foregroundColor(.textPrimary)
+            .fontWeight(.regular)
+    }
+    
+    var checkBox: some View {
+        HStack {
+            Button {
+
+            } label: {
+                if isCheckboxActive {
+                    Image(systemName: "checkmark.square.fill")
+                        .foregroundColor(.primaryPurple)
+                } else {
+                    Image(systemName: "square")
+                        .foregroundColor(.textField)
+                }
+            }
+            
+            Text("기억이 나지 않아요")
+                .foregroundColor(.textField)
+                .font(.footnote)
+        }
+    }
+}
+
+//struct BasicInfoContent_Previews: PreviewProvider {
+//    static var previews: some View {
+//        BasicInfoContent()
+//    }
+//}
--- GBZG/View/Write/WriteView.swift
+++ GBZG/View/Write/WriteView.swift
@@ -2,14 +2,130 @@
 //  WriteView.swift
 //  GBZG
 //
-//  Created by KiWoong Hong on 2022/11/15.
+//  Created by Noah's Ark on 2023/01/10.
 //
 
 import SwiftUI
 
 struct WriteView: View {
+    @ObservedObject var viewModel = WriteViewModel()
+    @State private var date = Date()
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            VStack {
+                themeTitle
+                basicInfoContainer
+                customInfoContainer
+                Spacer()
+            }
+            .padding(.horizontal)
+            .sheet(isPresented: $viewModel.isSearchViewActive) {
+                Text("SearchView Here")
+            }
+
+            calendarView
+        }
+    }
+}
+
+private extension WriteView {
+    var themeTitle: some View {
+        HStack {
+            Text("어떤 테마를 기록하시나요?")
+                .foregroundColor(.textField)
+                .font(.title)
+                .fontWeight(.bold)
+            Spacer()
+        }
+        .padding(.top, 28)
+        .onTapGesture {
+            viewModel.didTapTitleButton()
+        }
+    }
+    
+    var basicInfoContainer: some View {
+        VStack {
+            HStack {
+                Text("기본정보")
+                Spacer()
+                Image(systemName: "chevron.down")
+                    .rotationEffect(Angle(
+                        degrees: viewModel.isBasicInfoContainerActive ? 180 : 0
+                    ))
+            }
+            .foregroundColor(.textPrimary)
+            .font(.subheadline)
+            .onTapGesture {
+                viewModel.didTapBasicInfoButton()
+            }
+            
+            HStack {
+                VStack(alignment: .leading) {
+                    BasicInfoContent(title: "방문일") {
+                        Text("\(Date().formatted())")
+                            .foregroundColor(.textSecondary)
+                            .fontWeight(.regular)
+                            .onTapGesture {
+                                viewModel.didCalendarTapped()
+                            }
+                    }
+                }
+                Spacer()
+            }
+            .frame(height: viewModel.isBasicInfoContainerActive ? 75 : 0)
+            .opacity(viewModel.isBasicInfoContainerActive ? 1 : 0)
+        }
+        .padding(.top, 24)
+        .padding(.bottom)
+    }
+    
+    var customInfoContainer: some View {
+        VStack {
+            HStack {
+                Text("추가정보")
+                Spacer()
+                Image(systemName: "chevron.down")
+                    .rotationEffect(Angle(
+                        degrees: viewModel.isCustomInfoContainerActive ? 180 : 0
+                    ))
+            }
+            .foregroundColor(.textPrimary)
+            .font(.subheadline)
+            .onTapGesture {
+                viewModel.didTapCustomInfoButton()
+            }
+            
+            VStack {
+                Text("Hello World")
+                Text("Hello World")
+                Text("Hello World")
+            }
+            .frame(height: viewModel.isCustomInfoContainerActive ? 35 : 0)
+            .opacity(viewModel.isCustomInfoContainerActive ? 1 : 0)
+        }
+    }
+    
+    var calendarView: some View {
+        VStack {
+            if viewModel.isCalendarActive {
+                DatePicker(
+                    "Start Date",
+                    selection: $date,
+                    displayedComponents: [.date]
+                )
+                .datePickerStyle(.graphical)
+                .padding()
+                .background(.white)
+                .cornerRadius(13)
+                .padding(.horizontal)
+            }
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(viewModel.isCalendarActive ? .black.opacity(0.2) : .clear)
+//        .onTapGesture {
+//            viewModel.didCalendarTapped()
+//        }
     }
 }
 
@@ -18,3 +134,4 @@ struct WriteView: View {
 //        WriteView()
 //    }
 //}
+
