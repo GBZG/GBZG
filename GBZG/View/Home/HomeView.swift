dd3817a93f5cb5e91c4bf34e7008ca943b5e4e64
otoolz <otool.hf@gmail.com>
2023-01-11 18:46:07 +0900

    [Feat] HomeView 구조 작성 ( #7 )
    HomeView 기본 구조를 작성했습니다.

--- GBZG/View/Home/HomeView.swift
+++ GBZG/View/Home/HomeView.swift
@@ -9,7 +9,45 @@ import SwiftUI
 
 struct HomeView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            //ScrollView {
+            VStack(alignment: .leading) {
+                ScrollView {
+                    myCharacterContainer
+                    lifeThemeContainer
+                    escapeLogContainer
+                }
+            }
+            .padding(16)
+            .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .leading)
+                .navigationTitle("롬희의 방탈출")
+                .toolbar {
+                    ToolbarItem {
+                        NavigationLink(destination: Text("good")) {
+                            Image(systemName: "gearshape.fill")
+                                .tint(.textField)
+                        }
+                    }
+                }
+        }
+    }
+    
+    var myCharacterContainer: some View {
+        VStack {
+            Text("124방")
+        }
+    }
+    
+    var lifeThemeContainer: some View {
+        VStack {
+            Text("인생테마")
+        }
+    }
+
+    var escapeLogContainer: some View {
+        VStack {
+            Text("탈출일지")
+        }
     }
 }
 
