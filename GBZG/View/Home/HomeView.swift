7671786ee838ded5ea5c3b8359f4ae08031b7a2e
otoolz <otool.hf@gmail.com>
2023-01-11 22:18:35 +0900

    [Feat] HomeView 탈출일지 부분 UI ( #7 )
    HomeView 탈출일지 부분 UI 작성

--- GBZG/View/Home/HomeView.swift
+++ GBZG/View/Home/HomeView.swift
@@ -8,46 +8,106 @@
 import SwiftUI
 
 struct HomeView: View {
+    
+    let escapeLogItemImageSize = (UIScreen.main.bounds.size.width - 32) / 5
+    
     var body: some View {
         NavigationView {
-            //ScrollView {
             VStack(alignment: .leading) {
-                ScrollView {
+                ScrollView(showsIndicators: false) {
                     myCharacterContainer
                     lifeThemeContainer
-                    escapeLogContainer
+                    escapeLogListContainer
                 }
             }
             .padding(16)
             .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .leading)
-                .navigationTitle("롬희의 방탈출")
-                .toolbar {
-                    ToolbarItem {
-                        NavigationLink(destination: Text("good")) {
-                            Image(systemName: "gearshape.fill")
-                                .tint(.textField)
-                        }
+            .navigationTitle("롬희의 방탈출")
+            .toolbar {
+                ToolbarItem {
+                    NavigationLink(destination: Text("good")) {
+                        Image(systemName: "gearshape.fill")
+                            .tint(.textField)
                     }
                 }
+            }
         }
     }
-    
+}
+
+extension HomeView {
     var myCharacterContainer: some View {
         VStack {
             Text("124방")
         }
     }
-    
+}
+
+extension HomeView {
     var lifeThemeContainer: some View {
         VStack {
             Text("인생테마")
         }
     }
+}
 
-    var escapeLogContainer: some View {
+extension HomeView {
+    var escapeLogListContainer: some View {
         VStack {
-            Text("탈출일지")
+            HStack {
+                Text("탈출일지")
+                    .GBZGTitle1()
+                Spacer()
+            }
+            .padding(.bottom, 16)
+            
+            HStack {
+                Text("52 리뷰")
+                Spacer()
+                Text("최근 방문일 순")
+                Image(systemName: "chevron.down")
+            }
+            .GBZGBody2()
+            .foregroundColor(.secondary)
+            .padding(.horizontal, 7)
+            .padding(.bottom, 16)
+            
+            VStack {
+                ForEach(Range(0...5)) { num in
+                    NavigationLink(destination: Text("DetailView")) {
+                        escapeLogCell
+                    }
+                }
+            }
         }
+    }
+    
+    var escapeLogCell: some View {
+        HStack {
+            Image("Bin")
+                .resizable()
+                .frame(width: escapeLogItemImageSize, height: escapeLogItemImageSize)
+                .padding(.trailing, 16)
+            VStack {
+                HStack {
+                    Text("단디해라")
+                    Image(systemName: "leaf")
+                    Spacer()
+                }
+                .GBZGHeadline()
+                .tint(.primary)
+                .padding(.bottom, 4)
+                HStack {
+                    Image(systemName: "leaf")
+                    Text("지구별 방탈출 | 22.06.21")
+                    Spacer()
+                }
+                .GBZGBody2()
+                .tint(.secondary)
+            }
+            Spacer()
+        }
+        .padding(.bottom, 16)
     }
 }
 
