120ef19b08a3beb7052b4da9765dc90e25788fc3
stevesim <steve152510@gmail.com>
2023-01-19 00:05:18 +0900

    [Feat] RankingView

--- GBZG/View/Component/RankingList.swift
+++ GBZG/View/Component/RankingList.swift
@@ -12,31 +12,31 @@ struct RankingList: View {
     let title: String
     let location: String
     
+    
     var body: some View {
         
         HStack(spacing: 16){
             // 랭킹
             Text(String(rank))
-                .font(.system(size: 20, weight: .semibold))
+                .gbzgHeadline()
                 .foregroundColor(.textPrimary)
             
             // 이미지
-            Rectangle()
+            RoundedRectangle(cornerRadius: 12)
                 .fill(Color.primaryPurple)
                 .frame(width: 72, height: 72)
-                .cornerRadius(12)
             
             // 테마 정보
             VStack(alignment: .leading, spacing: 4){
                 Text(title)
-                    .font(.system(size: 20, weight: .semibold))
+                    .gbzgHeadline()
                     .foregroundColor(.textPrimary)
                 HStack(spacing: 4){
                     Image(systemName: "paperplane.fill")
-                        .font(.system(size: 15, weight: .regular))
+                        .gbzgBody2()
                         .foregroundColor(.textSecondary)
                     Text(location)
-                        .font(.system(size: 15, weight: .regular))
+                        .gbzgBody2()
                         .foregroundColor(.textSecondary)
                 }
             
