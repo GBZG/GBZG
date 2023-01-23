fb61832e905030b963a019153e2b2d1ddde0b52b
Timo Nam <dwbh7358@gmail.com>
2023-01-24 00:57:45 +0900

    [Style] RecentThemeRow 익스텐션 사용 리팩터링

--- GBZG/View/Write/Search/RecentThemeRow.swift
+++ GBZG/View/Write/Search/RecentThemeRow.swift
@@ -7,6 +7,7 @@
 
 import SwiftUI
 
+// TODO: 임시 Model, Integration 하면서 뺄 부분
 struct TmpRecentThemeRowModel: Identifiable {
     let id: UUID
     let title: String
@@ -20,50 +21,57 @@ struct TmpRecentThemeRowModel: Identifiable {
 }
 
 struct RecentThemeRow: View {
+    // TODO: 임시 Model, Integration 하면서 뺄 부분
     let model: TmpRecentThemeRowModel
     
     var body: some View {
         HStack(alignment: .top) {
             Button(action: {}) {
                 HStack(spacing: 16) {
-                    Rectangle()
-                        .foregroundColor(.primaryPurple)
-                        .frame(width: 76, height: 104)
-                        .cornerRadius(12)
-                    
-                    VStack(alignment: .leading, spacing: 12) {
-                        Text(model.title)
-                            .foregroundColor(.textPrimary)
-                            .gbzgHeadline()
-                        
-                        HStack(spacing: 4) {
-                            Image("Location")
-                                .resizable()
-                                .frame(width: 18, height: 18)
-                            
-                            Text(model.location)
-                                .gbzgBody2()
-                        }
-                        .foregroundColor(.textSecondary)
-                        
-                        Spacer()
-                    }
-                    
+                    PosterImage(url: nil)
+                    TextInfo(title: model.title, location: model.location)
                     Spacer()
                 }
             }
             .frame(minWidth: 100, maxHeight: 104)
-            
             Spacer()
-            
-            Button(action: {}) {
-                Image(systemName: "xmark")
-                    .resizable()
-                    .frame(width: 15.73, height: 15.73)
-            }
-            .foregroundColor(.textField)
-            .frame(width: 24, height: 24)
+            XmarkButton(action: {})
         }
+    }
+}
+
+private extension RecentThemeRow {
+    func PosterImage(url: URL?) -> some View {
+        Rectangle()
+            .foregroundColor(.primaryPurple)
+            .frame(width: 76, height: 104)
+            .cornerRadius(12)
+    }
+    
+    func TextInfo(title: String, location: String) -> some View {
+        VStack(alignment: .leading, spacing: 12) {
+            Text(title)
+                .foregroundColor(.textPrimary)
+                .gbzgHeadline()
+            HStack(spacing: 4) {
+                Image("Location")
+                    .resizable()
+                    .frame(width: 18, height: 18)
+                Text(location)
+                    .gbzgBody2()
+            }
+            .foregroundColor(.textSecondary)
+        }
+    }
+    
+    func XmarkButton(action: () -> Void) -> some View {
+        Button(action: {}) {
+            Image(systemName: "xmark")
+                .resizable()
+                .frame(width: 15.73, height: 15.73)
+        }
+        .foregroundColor(.textField)
+        .frame(width: 24, height: 24)
     }
 }
 
