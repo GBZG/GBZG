d0905512e557f92e5954f43123c8523a8ecd01e6
otoolz <otool.hf@gmail.com>
2023-03-08 16:45:47 +0900

    [Feat] TemplateView Body Container UI 작성
    TemplateView Body Container UI 작성

--- GBZG/View/Template/TemplateView.swift
+++ GBZG/View/Template/TemplateView.swift
@@ -9,12 +9,19 @@ import SwiftUI
 
 struct TemplateView: View {
     
+    let itemWidth = (UIScreen.main.bounds.size.width - 32)
+    var items: [Int] = [1,2,3,4,5]
+    
     var body: some View {
         NavigationView {
-            VStack(alignment: .leading) {
-                ScrollView(showsIndicators: false) {
-                    TemplateViewHeader
+            VStack(alignment: .center) {
+                TemplateViewHeader
+                if items.count == 0 {
+                    EmptyListContainer
+                } else {
+                    ItemsListContainer
                 }
+                Spacer()
             }
             .padding(16)
             .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .leading)
@@ -38,6 +45,49 @@ private extension TemplateView {
             }
         }
         .padding(.bottom, 12)
+    }
+    
+    var ItemsListContainer: some View {
+        ScrollView(showsIndicators: false) {
+            ForEach(items, id: \.self) { item in
+                NavigationLink(destination: Text("\(item)")) {
+                    ItemCell(title: "item.title", components: ["item.component1", "item.component2"])
+                }
+            }
+        }
+    }
+    
+    func ItemCell(title: String, components: [String]) -> some View {
+        VStack(alignment: .leading) {
+            HStack {
+                Text(title)
+                    .tint(.textPrimary)
+                Spacer()
+            }
+            .padding(.top, 16)
+            .padding(.bottom, 8)
+            .padding(.horizontal, 16)
+            
+            Text(components.joined(separator: " "))
+                .tint(.textSecondary)
+                .padding(.horizontal, 16)
+                .padding(.bottom, 16)
+        }
+        .frame(width: itemWidth)
+        .gbzgBody2()
+        .background(Color.textField)
+        .cornerRadius(12)
+        .padding(.bottom, 12)
+    }
+    
+    var EmptyListContainer: some View {
+        VStack {
+            Spacer()
+            Text("아직 저장된 템플릿이 없어요!")
+                .gbzgBody2()
+                .tint(.textSecondary)
+            Spacer()
+        }
     }
 }
 
