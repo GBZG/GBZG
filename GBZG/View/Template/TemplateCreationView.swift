2732a176dde8393fe0e6e850803541a4ba3eef12
otoolz <otool.hf@gmail.com>
2023-04-17 14:24:38 +0900

    [Feat] 구성요소 파트 추가
    구성요소 파트 추가

--- GBZG/View/Template/TemplateCreationView.swift
+++ GBZG/View/Template/TemplateCreationView.swift
@@ -7,12 +7,33 @@
 
 import SwiftUI
 
+struct Component: Hashable {
+    var name: String
+    var isChecked: Bool = false
+    var listNumber: Int = 0
+}
+
 struct TemplateCreationView: View {
+    
     @State var text = ""
+    @State var allComponents: [Component] = [
+        Component(name: "대충"),
+        Component(name: "템플릿의"),
+        Component(name: "구성"),
+        Component(name: "요소"),
+        Component(name: "이름입니다"),
+        Component(name: "하하"),
+        Component(name: "호호"),
+        Component(name: "쿄쿄"),
+        Component(name: "후후")
+    ]
+    
     var body: some View {
         VStack {
             TemplateCreationViewHeader
             TemplateNameEditContainer
+            TemplateComponentsEditContainer
+            AddOptionButton
             Spacer()
         }
         .padding(16)
@@ -26,7 +47,7 @@ extension TemplateCreationView {
                 .gbzgLargeTitle()
             Spacer()
         }
-        .padding(.bottom, 12)
+        .padding(.bottom, 16)
     }
     
     var TemplateNameEditContainer: some View {
@@ -50,23 +71,57 @@ extension TemplateCreationView {
                 .frame(height: 0.8)
                 .foregroundColor(text == "" ? .textPrimary : .purple030)
         }
+        .padding(.bottom, 36)
     }
     
     var TemplateComponentsEditContainer: some View {
-        Text("hi")
-        // TemplateComponentItem:
-        /*
-        ScrollView(showsIndicators: false) {
-            ForEach(items, id: \.self) { item in
-                NavigationLink(destination: Text("\(item)")) {
-                    ItemCell(title: "item.title", components: ["item.component1", "item.component2"])
-                        .padding(.bottom, 12)
-                }
+        VStack {
+            HStack {
+                Text("구성 요소")
+                    .gbzgSubtitle()
+                Spacer()
+            }
+            .padding(.bottom, 16)
+            ForEach($allComponents, id: \.self) { $component in
+                TemplateComponentsItem($component)
             }
         }
-         */
     }
     
+    func TemplateComponentsItem(_ item: Binding<Component>) -> some View {
+        HStack {
+            Image(systemName: item.isChecked.wrappedValue ? "\(item.listNumber.wrappedValue).square.fill" : "square")
+                .foregroundColor(item.isChecked.wrappedValue ? .purple030 : .black)
+                .onTapGesture {
+                    item.isChecked.wrappedValue.toggle()
+                    if item.isChecked.wrappedValue {
+                        item.listNumber.wrappedValue = allComponents.filter({ $0.isChecked }).count
+                    } else {
+                        allComponents.enumerated().forEach { (index, component) in
+                            if component.listNumber > item.listNumber.wrappedValue {
+                                allComponents[index].listNumber -= 1
+                            }
+                        }
+                    }
+                }
+            Text(item.name.wrappedValue)
+            Spacer()
+        }
+        .gbzgBody1()
+        .padding(.bottom, 16)
+    }
+    
+    var AddOptionButton: some View {
+        Button(action: {
+        }, label: {
+            HStack {
+                Image(systemName: "plus")
+                Text("옵션 추가")
+                Spacer()
+            }
+            .foregroundColor(.textPrimary)
+        })
+    }
 }
 
 struct TemplateCreationView_Previews: PreviewProvider {
